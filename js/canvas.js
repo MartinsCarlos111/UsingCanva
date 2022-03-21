10e7d7fbcf05c6b16019f7f0d2e3dd5dbf37dae8
Davi Gonzaga <72495823+Davignz@users.noreply.github.com>
2022-03-21 10:49:38 -0300

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -2,64 +2,49 @@ var canva = document.getElementById("areaCanv"), ctx = canva.getContext("2d");
 var img = document.getElementById("fundo");
 ctx.drawImage(img, 0, 0);
 
-//Função nova
-ctx.beginPath();
-ctx.bezierCurveTo(415, 600, 550, 450, 748, 600);
-ctx.fillStyle = 'white';
+//laguinho
 
-//Montanha 1
+ctx.beginPath();
+ctx.arc(250, 1100, 550, 2*3,14);
+ctx.fillStyle = "rgb(112, 146, 190)";
+ctx.fill();
+
+
+ctx.moveTo(1300, 800);
+ctx.lineTo(1100, 300);
+ctx.lineTo(699, 1100);
+
+//montanha 1
 ctx.moveTo(1300, 500);
 ctx.lineTo(1200, 300)
 ctx.moveTo(1150, 423);
 ctx.lineTo(1200, 300);
 
-//Montanha 2 > direita p esquerda
-ctx.moveTo(1300, 800);
-ctx.lineTo(1100, 300);
-ctx.lineTo(699, 1100);
 
-//Montanha 3
+//montanha 3
 ctx.moveTo(950, 400);
 ctx.lineTo(1000, 500);
 ctx.moveTo(950, 400);
-ctx.lineTo(775, 670);
+ctx.lineTo(710, 800);
 
-//Montanha 4
+//montanha 4
 ctx.moveTo(850, 450);
-ctx.lineTo(700, 670);
+ctx.lineTo(700, 650);
 ctx.moveTo(850, 450);
-ctx.lineTo(900, 480);
+ctx.lineTo(890, 500);
 
-//A esquerda Montanha 1
-ctx.moveTo(130, 150);
-ctx.lineTo(0, 400);
-ctx.moveTo(180, 240);
-ctx.lineTo(130, 150);
-ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-ctx.fillRect (1300, 1300, 450, 150);
-
-//Montanha a esquerda 2
+//montanha a esquerda 1
 ctx.moveTo(0, 600);
 ctx.lineTo(200, 200);
 ctx.moveTo(200, 200);
-ctx.lineTo(450, 670);
-ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-ctx.fillRect (0, 600, 450, 70);
+ctx.lineTo(350, 400);
 
-//Linha 
-ctx.moveTo(0, 670);
-ctx.lineTo(915, 670);
+//Montanha 2 esquerda
+ctx.moveTo(500, 100);
+ctx.lineTo(300, 500);
+ctx.moveTo(750, 584);
+ctx.lineTo(500, 100);
 
 
-//1º Estrada
-ctx.moveTo(0, 730);
-ctx.lineTo(100, 730);
-
-//2º Estrada
-ctx.moveTo(700, 730);
-ctx.lineTo(300, 730);
-ctx.fillStyle = "rgba(54,54,54,0.8)";
-ctx.fillRect (0, 670, 915, 300);
-
-ctx.strokeStyle = "black";
-ctx.stroke(); 
+ctx.strokeStyle = "#fff";
+ctx.stroke();
