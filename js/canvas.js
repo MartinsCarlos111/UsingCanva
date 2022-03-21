bcd6794032adb8342e672dad53104173abca30fb
Davi Gonzaga <72495823+Davignz@users.noreply.github.com>
2022-03-21 10:51:05 -0300

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -1,50 +1,75 @@
 var canva = document.getElementById("areaCanv"), ctx = canva.getContext("2d");
-var img = document.getElementById("fundo");
-ctx.drawImage(img, 0, 0);
 
-//laguinho
+//Função nova
 
-ctx.beginPath();
-ctx.arc(250, 1100, 550, 2*3,14);
-ctx.fillStyle = "rgb(112, 146, 190)";
+ctx.bezierCurveTo(415, 600, 550, 450, 748, 600);
+ctx.fillStyle = '#FFFF00';
 ctx.fill();
+ctx.stroke();
 
+//Montanha 1
+ctx.fillStyle = 'black';
+ctx.strokeStyle = "red";
+ctx.beginPath();
+ctx.moveTo(1300, 500);
+ctx.lineTo(1200, 300);
+ctx.lineTo(1100, 600);
+ctx.lineTo(1300, 800);
+ctx.fill();
+ctx.stroke();
 
+//Montanha 2 > direita p esquerda
+ctx.fillStyle = "black";
+ctx.beginPath();
 ctx.moveTo(1300, 800);
 ctx.lineTo(1100, 300);
-ctx.lineTo(699, 1100);
+ctx.lineTo(700, 1100);
+ctx.stroke();
+ctx.fill();
 
-//montanha 1
-ctx.moveTo(1300, 500);
-ctx.lineTo(1200, 300)
-ctx.moveTo(1150, 423);
-ctx.lineTo(1200, 300);
-
-
-//montanha 3
+//Montanha 3
+ctx.fillStyle = "black";
+ctx.beginPath();
 ctx.moveTo(950, 400);
 ctx.lineTo(1000, 500);
-ctx.moveTo(950, 400);
-ctx.lineTo(710, 800);
+ctx.lineTo(900, 650);
+ctx.stroke();
 
-//montanha 4
+//Montanha 4
 ctx.moveTo(850, 450);
-ctx.lineTo(700, 650);
+ctx.lineTo(700, 670);
 ctx.moveTo(850, 450);
-ctx.lineTo(890, 500);
+ctx.lineTo(900, 480);
+ctx.stroke();
 
-//montanha a esquerda 1
+// Montanha 1
+ctx.moveTo(130, 150);
+ctx.lineTo(0, 400);
+ctx.moveTo(180, 240);
+ctx.lineTo(130, 150);
+ctx.stroke();
+
+//Montanha a esquerda 2
 ctx.moveTo(0, 600);
 ctx.lineTo(200, 200);
 ctx.moveTo(200, 200);
-ctx.lineTo(350, 400);
-
-//Montanha 2 esquerda
-ctx.moveTo(500, 100);
-ctx.lineTo(300, 500);
-ctx.moveTo(750, 584);
-ctx.lineTo(500, 100);
-
-
-ctx.strokeStyle = "#fff";
+ctx.lineTo(450, 670);
 ctx.stroke();
+
+//Linha 
+ctx.moveTo(0, 670);
+ctx.lineTo(915, 670);
+ctx.stroke();
+
+//1º Estrada
+ctx.strokeStyle = "red";
+ctx.stroke();
+ctx.moveTo(0, 730);
+ctx.lineTo(100, 730);
+
+//2º Estrada
+ctx.moveTo(700, 730);
+ctx.lineTo(300, 730);
+ctx.stroke();
+ctx.strokeStyle = "white";
+ctx.stroke(); 
